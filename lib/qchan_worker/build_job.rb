7e1a7ece7984efb8328e534531ef8a5dfa740da1
Ryo Nakamura <r7kamura@gmail.com>
2013-12-22 10:42:15 +0900

    BuildJob executes given shell script

--- lib/qchan_worker/build_job.rb
+++ lib/qchan_worker/build_job.rb
@@ -1,6 +1,35 @@
+require "open3"
+
 module QchanWorker
   class BuildJob
-    def self.perform(attributes)
+    def self.perform(*args)
+      new(*args).perform
+    end
+
+    def initialize(attributes)
+      @attributes = attributes
+    end
+
+    def perform
+      { output: output, status: status.success? }
+    end
+
+    private
+
+    def command
+      "set -e; "+ @attributes["command"].gsub(/\r\n|\n/, ";")
+    end
+
+    def output
+      result[0]
+    end
+
+    def status
+      result[1]
+    end
+
+    def result
+      @result ||= Open3.capture2e(command)
     end
   end
 end
