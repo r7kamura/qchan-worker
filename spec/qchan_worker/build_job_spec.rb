4516856aec46c5f4a4ba0f2b26c979d459e069aa
Ryo Nakamura <r7kamura@gmail.com>
2013-12-22 11:03:34 +0900

    Add more tests

--- spec/qchan_worker/build_job_spec.rb
+++ spec/qchan_worker/build_job_spec.rb
@@ -3,11 +3,40 @@ require "spec_helper"
 describe QchanWorker::BuildJob do
   describe ".perform" do
     let(:attributes) do
-      { "command" => "echo hello world" }
+      { "command" => command }
     end
 
-    it "executes given shell-script and returns its output & exit status" do
-      described_class.perform(attributes).should == { output: "hello world\n", status: 0 }
+    let(:command) do
+      "echo hello world"
+    end
+
+    context "with error" do
+      let(:command) do
+        "echo hello; fail; echo world"
+      end
+
+      it "stops there" do
+        result = described_class.perform(attributes)
+        result[:status].should_not == 0
+        result[:output].should_not include("world")
+        result[:output].should include("fail")
+      end
+    end
+
+    context "with multiline command" do
+      let(:command) do
+        "echo hello\necho world"
+      end
+
+      it "succeeds" do
+        described_class.perform(attributes).should == { output: "hello\nworld\n", status: 0 }
+      end
+    end
+
+    context "with valid condition" do
+      it "executes given shell-script and returns its output & exit status" do
+        described_class.perform(attributes).should == { output: "hello world\n", status: 0 }
+      end
     end
   end
 end
