50073dc9f7bcbae7c53fd6c67e88d61388ebf086
Ryo Nakamura <r7kamura@gmail.com>
2013-12-24 03:29:23 +0900

    Stub redis connection at Publisher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,9 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+
+  # Stub Redis.
+  config.before do
+    QchanWorker::Publisher.stub(:publish)
+  end
 end
