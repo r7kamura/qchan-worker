664c50cdc7e3cdc824b660e3a2d495161695bfcc
Ryo Nakamura <r7kamura@gmail.com>
2013-12-24 03:47:51 +0900

    Docker workspace option must be absolute path

--- lib/qchan_worker/builder/executor.rb
+++ lib/qchan_worker/builder/executor.rb
@@ -34,7 +34,7 @@ module QchanWorker
 
       def script
         str = "/bin/bash -e #{tempfile.path}"
-        str = "docker run -v #{tempdir}:/#{workspace} -w #{workspace} #{image} #{str}" if has_docker?
+        str = "docker run -v #{tempdir}:#{workspace} -w #{workspace} #{image} #{str}" if has_docker?
         str
       end
 
@@ -73,7 +73,7 @@ module QchanWorker
       end
 
       def workspace
-        "workspace"
+        "/workspace"
       end
     end
   end
